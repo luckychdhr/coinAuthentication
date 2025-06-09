adf813337b90a70fb5ef8507977dbc0c2f452786
Lucky Choudhary <luckychoudhary@Luckys-MacBook-Air.local>
2025-06-09 19:33:37 +0530

    Error resolved

--- src/components/verify/VerificationForm.tsx
+++ src/components/verify/VerificationForm.tsx
@@ -442,17 +442,7 @@ export default function VerificationForm() {
           },
           explorerRecommendedWalletIds: [
             "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", //trust wallet
-            "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // MetaMask
-            "8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4", // Binance
-            "971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709", // OKX
-            "15c8b91ade1a4e58f3ce4e7a0dd7f42b47db0c8df7e0d84f63eb39bcb96c4e0f", // Bybit Wallet
-            "fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa", // CoinBase
-            "38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662", // BitGet
-            "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393", // Phantom
-            "0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150", // SafePal
-            "c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a", //ledger live
           ],
-          explorerExcludedWalletIds: "ALL",
         },
       }),
     []
