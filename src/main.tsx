79cf0d4e980ca5d758ea3d67d96f01c9c11993fd
Lucky Choudhary <luckychoudhary@Luckys-MacBook-Air.local>
2025-06-14 19:50:38 +0530

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react'
 
 // Use createRoot API for React 18
 createRoot(document.getElementById('root')!).render(
